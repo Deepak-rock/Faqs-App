9661b41c098559569bdf7cd59aea54e0547b101e
Deepak-rock <deepakind777@gmail.com>
2024-09-09 14:39:23 +0530

    Test1

--- src/components/FaqItem/index.js
+++ src/components/FaqItem/index.js
@@ -1,1 +1,39 @@
 // Write your code here.
+import {Component} from 'react'
+import './index.css'
+
+class FaqItem extends Component {
+  state = {isShowAnswer: false}
+
+  toggleIcon = () => {
+    const {isShowAnswer} = this.state
+    this.setState({isShowAnswer: !isShowAnswer})
+  }
+
+  render() {
+    const {faqDetails} = this.props
+    const {questionText, answerText} = faqDetails
+    const {isShowAnswer} = this.state
+    const iconUrl = isShowAnswer
+      ? 'https://assets.ccbp.in/frontend/react-js/faqs-minus-icon-img.png'
+      : 'https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png'
+    const iconAlt = isShowAnswer ? 'minus' : 'plus'
+    return (
+      <li className="faqs-item">
+        <div className="qus-ans-container">
+          <h1 className="question">{questionText}</h1>
+          <button className="button" type="button" onClick={this.toggleIcon}>
+            <img src={iconUrl} alt={iconAlt} className="button-icon" />
+          </button>
+        </div>
+        {isShowAnswer && (
+          <>
+            <hr />
+            <p className="answer">{answerText}</p>
+          </>
+        )}
+      </li>
+    )
+  }
+}
+export default FaqItem
--- src/components/Faqs/index.js
+++ src/components/Faqs/index.js
@@ -1,1 +1,20 @@
 // Write your code here.
+import FaqItem from '../FaqItem/index'
+import './index.css'
+
+const Faqs = props => {
+  const {faqsList} = props
+  return (
+    <div className="app-container">
+      <div className="faqs-container">
+        <h1 className="heading">FAQs</h1>
+        <ul className="faqs-list">
+          {faqsList.map(faqItem => (
+            <FaqItem key={faqItem.id} faqDetails={faqItem} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+export default Faqs
